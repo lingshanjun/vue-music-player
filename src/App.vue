e92bd34050f86a9793cbf638527298b8ca6c0a8e
lwj <1198275198@qq.com>
2017-08-23 23:24:41 +0800

    Feat: 路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <!-- <router-view></router-view> -->
     <m-header></m-header>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-// import router from './router';
+import router from './router';
 import fastclick from 'fastclick';
 
 import '@common/scss/index.scss';
@@ -13,7 +13,7 @@ fastclick.attach(document.body);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   template: '<App/>',
   components: {
     App

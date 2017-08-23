86f45c41d1e30a4a8cc6836df8af05f4cad47d79
lwj <1198275198@qq.com>
2017-08-23 17:43:36 +0800

    Chore: 项目调整
    1. 调整项目结构
    2. 使用scss
    3. 加载通用fonts css资源
    4. eslint 配置必须使用句末分号

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    hello music
   </div>
 </template>
 
 <script>
 export default {
   name: 'app'
-}
+};
 </script>
 
-<style>
+<style lang='scss'>
+@import 'common/scss/variable.scss';
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $color-theme;
 }
 </style>

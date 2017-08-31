443ff4a34149eb917d340cbdd11843097f84b99a
lwj <1198275198@qq.com>
2017-08-31 10:22:49 +0800

    Feat: 播放相关的state数据定义

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,14 @@
+import { playMode } from '@assets/js/config';
+
 const state = {
-  singer: {}
+  singer: {}, // 歌手
+
+  playing: false, // 播放状态
+  fullScreen: false, // 播放全屏状态
+  playList: [], // 播放列表
+  sequencelist: [], // 原始播放列表
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1 // 当前播放歌曲索引
 };
 
 export default state;

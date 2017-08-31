86a867e8be76f23fe4e70f80e08bc1052d922c64
lwj <1198275198@qq.com>
2017-08-31 11:59:53 +0800

    Feat: player样式

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,10 +1,80 @@
 <template>
   <div class="player" v-show="playList.length">
     <div class="normal-player" v-show="fullScreen">
-      normal-player
+      <div class="background">
+        <img src="" alt="" width="100%" height="100%">
+      </div>
+      <div class="top">
+        <div class="back">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title">标题</h1>
+        <h2 class="subtitle">副标题</h2>
+      </div>
+      <div class="middle">
+        <div class="middle-l">
+          <div class="cd-wrapper">
+            <div class="cd">
+              <img class="image" src="" alt="">
+            </div>
+          </div>
+          <div class="playing-lyric-wrapper">
+            <div class="playing-lyric">播放歌词</div>
+          </div>
+        </div>
+        <div class="middle-r">
+          <div class="lyric-wrapper">
+            <div>
+              <p class="text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="dot-wrapper">
+          <span class="dot"></span>
+          <span class="dot"></span>
+        </div>
+        <div class="progress-wrapper">
+          <span class="time time-l"></span>
+          <div class="progress-bar-wrapper">
+            <!-- progress bar -->
+          </div>
+          <span class="time time-r"></span>
+        </div>
+        <div class="operators">
+          <div class="icon i-left">
+            <i></i>
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon i-center">
+            <i></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon"></i>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="mini-player" v-show="!fullScreen">
-      mini-player
+      <div class="icon">
+        <img src="" alt="" width="40" height="40">
+      </div>
+      <div class="text">
+        <h2 class="name">mingzi</h2>
+        <p class="desc">singer</p>
+      </div>
+      <div class="control">
+        <!-- process circle -->
+      </div>
+      <div class="control">
+        <i class="icon-playlist"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -33,6 +103,202 @@ export default {
     bottom: 0;
     z-index: 150;
     background-color: $color-background;
+    .background {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      opacity: 0.6;
+      filter: blur(20px);
+    }
+    .top {
+      position: relative;
+      margin-bottom: 25px;
+      .back {
+        position: absolute;
+        top: 0;
+        left: 6px;
+        z-index: 50;
+        .icon-back {
+          display: block;
+          padding: 9px;
+          font-size: $font-size-large-x;
+          color: $color-theme;
+          transform: rotate(-90deg);
+        }
+      }
+      .title {
+        width: 70px;
+        margin: 0 auto;
+        line-height: 40px;
+        text-align: center;
+        @include no-wrap();
+        font-size: $font-size-large;
+        color: $color-text;
+      }
+      .subtitle {
+        line-height: 20px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-text;
+      }
+    }
+    .middle {
+      position: fixed;
+      width: 100%;
+      top: 80px;
+      bottom: 170px;
+      white-space: nowrap;
+      font-size: 0;
+      .middle-l {
+        position: relative;
+        display: inline-block;
+        width: 100%;
+        height: 0;
+        padding-top: 80%;
+        vertical-align: top;
+        .cd-wrapper {
+          position: absolute;
+          top: 0;
+          left: 10%;
+          width: 80%;
+          height: 100%;
+          .cd {
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            border: 10px solid rgba(255, 255, 255, .1);
+            border-radius: 50%;
+            &.play {
+              animation: rotate 20s linear infinite;
+            }
+            &.pause {
+              animation-play-state: paused;
+            }
+            .image {
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              border-radius: 50%;
+            }
+          }
+        }
+        .playing-lyric-wrapper {
+          width: 80%;
+          margin: 30px auto 0 auto;
+          overflow: hidden;
+          text-align: center;
+          .playing-lyric {
+            height: 20px;
+            line-height: 20px;
+            font-size: $font-size-medium;
+            color: $color-text-l;
+          }
+        }
+      }
+      .middle-r {
+        display: inline-block;
+        vertical-align: top;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        .lyric-wrapper {
+          width: 80%;
+          margin: 0 auto;
+          overflow: hidden;
+          text-align: center;
+          .text {
+            line-height: 32px;
+            color: $color-text-l;
+            font-size: $font-size-medium;
+            &.current {
+              color: $color-text;
+            }
+          }
+        }
+      }
+    }
+    .bottom {
+      position: absolute;
+      bottom: 50px;
+      width: 100%;
+      .dot-wrapper {
+        text-align: center;
+        font-size: 0;
+        .dot {
+          display: inline-block;
+          width: 8px;
+          height: 8px;
+          margin: 0 4px;
+          vertical-align: middle;
+          border-radius: 50%;
+          background-color: $color-text-l;
+          &.active {
+            width: 20px;
+            border-radius: 5px;
+            background-color: $color-text-ll;
+          }
+        }
+      }
+      .progress-wrappr {
+        display: flex;
+        align-items: center;
+        width: 80px;
+        margin: 0 auto;
+        padding: 10px 0;
+        .item {
+          flex: 0 0 30px;
+          width: 30px;
+          line-height: 30px;
+          color: $color-text;
+          font-size: $font-size-small;
+          &.item-l {
+            text-align: left;
+            ;
+          }
+          &.item-r {
+            text-align: right;
+          }
+        }
+        .progress-bar-wrapper {
+          flex: 1;
+        }
+      }
+      .operators {
+        display: flex;
+        align-items: center;
+        .icon {
+          flex: 1;
+          color: $color-theme;
+          &.disable {
+            color: $color-theme-d;
+          }
+          i {
+            font-size: 30px;
+          }
+        }
+        .icon-left {
+          text-align: right;
+        }
+        .icon-center {
+          padding: 0 20px;
+          text-align: center;
+          i {
+            font-size: 40px;
+          }
+        }
+        .icon-right {
+          text-align: left;
+        }
+        .icon-favorite {
+          color: $color-sub-theme;
+        }
+      }
+    }
   }
   .mini-player {
     position: fixed;
@@ -41,7 +307,68 @@ export default {
     width: 100%;
     height: 60px;
     z-index: 180;
+    display: flex;
+    align-items: center;
     background-color: $color-highlight-background;
+    .icon {
+      flex: 0 0 40px;
+      width: 40px;
+      padding: 0 10px 0 20px;
+      img {
+        border-radius: 50%;
+        &.play {
+          animation: rotate 10s linear infinite;
+        }
+        &.pause {
+          animation-play-state: pause;
+        }
+      }
+    }
+    .text {
+      display: flex;
+      flex-direction: columns;
+      justify-content: center;
+      flex: 1;
+      line-height: 20px;
+      overflow: hidden;
+      .name {
+        margin-bottom: 2px;
+        @include no-wrap();
+        font-size: $font-size-medium;
+        color: $color-text;
+      }
+      .desc {
+        @include no-wrap();
+        font-size: $font-size-small;
+        color: $color-text-d;
+      }
+    }
+    .control {
+      flex: 0 0 30px;
+      width: 30px;
+      padding: 0 10px;
+      .icon-play-mini,
+      .icon-pause-mini,
+      .icon-playlist {
+        font-size: 30px;
+        color: $color-theme-d;
+      }
+      .icon-mini {
+        font-size: 32px;
+        position: absolute;
+        left: 0;
+        top: 0;
+      }
+    }
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
